9636ea50a9499bc98101e5b7f24d474ff1134aa5
kissbarnabas57 <90251406+kissbarnabas57@users.noreply.github.com>
2021-10-05 10:26:35 +0200

    Add project files.

---
+++ Kémia/Elemek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kémia
+{
+  class Elemek
+  {
+    private string ev;
+
+    public string Ev
+    {
+      get { return ev; }
+      set { ev = value; }
+    }
+    private string elem;
+
+    public string Elem
+    {
+      get { return elem; }
+      set { elem = value; }
+    }
+    private string vegyjel;
+
+    public string Vegyjel
+    {
+      get { return vegyjel; }
+      set { vegyjel = value; }
+    }
+    private int rendszam;
+
+    public int Rendszam
+    {
+      get { return rendszam; }
+      set { rendszam = value; }
+    }
+    private string felfedezo;
+
+    public string Felfedezo
+    {
+      get { return felfedezo; }
+      set { felfedezo = value; }
+    }
+
+    public Elemek(string ev, string elem, string vegyjel, int rendszam, string felfedezo)
+    {
+      this.ev = ev;
+      this.elem = elem;
+      this.vegyjel = vegyjel;
+      this.rendszam = rendszam;
+      this.felfedezo = felfedezo;
+    }
+  }
+}
---
+++ Kémia/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kémia
+{
+  class Program
+  {
+    static List<string> lista = new List<string>();
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+    }
+
+    private static void OtodikFeladat()
+    {
+      Console.WriteLine("5. feladat: ");
+    }
+
+    private static void NegyedikFeladat()
+    {
+      int szam = 0;
+      foreach (var l in lista)
+      {
+        
+      }
+      Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {}");
+    }
+
+    private static void HarmadikFeladat()
+    {
+      Console.WriteLine($"3. feladat: Elemek száma: {lista.Count} ");
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader be = new StreamReader("felfedezesek.csv");
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        lista.Add( int.Parse(a[0], a[1], a[2], a[3], a[4]));
+      }
+      be.Close();
+    }
+  }
+}
